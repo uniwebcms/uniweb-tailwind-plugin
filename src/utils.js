90c7310bff0786260ecac145a0207d06b8406c98
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2025-10-24 16:30:16 -0400

    extends the color shades

--- src/utils.js
+++ src/utils.js
@@ -22,6 +22,7 @@ exports.generateElementConfig = (elementVars) => {
   elementVars.forEach((element) => {
     elementConfig[element] = `var(--${element})`;
 
+    // create shades from 0% to 100% in increments of 10%
     for (let i = 0; i <= 100; i += 10) {
       elementConfig[
         `${element}-${i}`
@@ -30,6 +31,22 @@ exports.generateElementConfig = (elementVars) => {
         `${element}/${i}`
       ] = `color-mix(in lch, var(--${element}) ${i}%, transparent);`;
     }
+
+    // explicitly add the 5% and 95% mix shades
+    elementConfig[
+      `${element}-5`
+    ] = `color-mix(in lch, var(--${element}) 5%, var(--light-mix));`;
+    elementConfig[
+      `${element}-95`
+    ] = `color-mix(in lch, var(--${element}) 95%, var(--light-mix));`;
+
+    // transparent shades
+    elementConfig[
+      `${element}-5`
+    ] = `color-mix(in lch, var(--${element}) 5%, transparent);`;
+    elementConfig[
+      `${element}-95`
+    ] = `color-mix(in lch, var(--${element}) 95%, transparent);`;
   });
 
   return elementConfig;
